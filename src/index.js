4f1a538916403d5a1434a478722e9f6815fde440
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-24 12:10:36 +0300

    feat: implement cwd before file manager start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { EOL } from 'os';
 import { createReadLineInterface } from './readline-interface/index.js';
 import { parseUserNameArg } from './utils/args.js';
+import { getUserHomeDir } from './utils/os.js';
 
 const runFileManager = () => {
     const userName = parseUserNameArg();
 
-    const rli = createReadLineInterface(userName);
+    createReadLineInterface(userName);
 
     console.log(`Welcome to the File Manager, ${userName}!${EOL}`);
+
+    console.log(`You are currently in ${process.cwd()}${EOL}`);
 };
+
+const userHomeDir = getUserHomeDir();
+
+process.chdir(userHomeDir);
 
 runFileManager();
