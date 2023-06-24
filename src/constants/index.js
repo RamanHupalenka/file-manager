31d47182cc88ab23973513314dfc3dd556e908ac
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-24 12:07:50 +0300

    fix: change reg exp's for commands

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const userNameParamPattern = '--username=';
-export const exitPattern = /\.exit/;
+export const exitPattern = /^\.exit$/;
 export const anonymousUserName = 'Anonymous';
 
-export const upCommandPattern = /^\s*up\s*$/;
-export const lsCommandPattern = /^\s*ls\s*$/;
+export const upCommandPattern = /^up$/;
+export const lsCommandPattern = /^ls$/;
 export const cdCommandPattern = /^(\s*cd\s+)+(.+)$/;
