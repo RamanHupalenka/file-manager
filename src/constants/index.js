e10eab80cf279069630bbbe651d50e03c28a87e9
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-24 11:19:32 +0300

    feat: implement nwd commands reg exp patterns

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,7 @@
 export const userNameParamPattern = '--username=';
 export const exitPattern = /\.exit/;
 export const anonymousUserName = 'Anonymous';
+
+export const upCommandPattern = /^\s*up\s*$/;
+export const lsCommandPattern = /^\s*ls\s*$/;
+export const cdCommandPattern = /^(\s*cd\s+)+(.+)$/;
