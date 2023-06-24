7d9e4b9e2e0d0523f1f4ec2d5fe6fb05b9b1f075
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-24 12:09:12 +0300

    feat: implement up command logic

---
+++ src/handlers/up-handler.js
@@ -0,0 +1,12 @@
+import { runUpCommand } from '../nwd/up.js';
+import { upCommandPattern } from '../constants/index.js';
+
+export const handleUpInput = (input) => {
+    const isCorrectUpCommandInput = upCommandPattern.test(input);
+
+    if (isCorrectUpCommandInput) {
+        runUpCommand();
+    } else {
+        console.log(`${EOL}Invalid input${EOL}`);
+    }
+};
--- src/readline-interface/index.js
+++ src/readline-interface/index.js
@@ -1,5 +1,7 @@
+import { EOL } from 'os';
 import { createInterface } from 'readline/promises';
 import { handleExitInput } from '../handlers/exit-handler.js';
+import { handleUpInput } from '../handlers/up-handler.js';
 import { getFinalMessageToUser } from '../utils/string.js';
 
 export const createReadLineInterface = (userName) => {
@@ -9,7 +11,21 @@ export const createReadLineInterface = (userName) => {
     });
 
     rli.on('line', (input) => {
-        handleExitInput(input, userName);
+        const formattedInput = input.trim();
+
+        switch (true) {
+            case formattedInput.startsWith('.exit'):
+                handleExitInput(formattedInput, userName);
+                break;
+            case formattedInput.startsWith('up'):
+                handleUpInput(formattedInput);
+                break;
+            default:
+                console.log(`${EOL}Invalid input${EOL}`);
+                break;
+        }
+
+        console.log(`You are currently in ${process.cwd()}${EOL}`);
     });
 
     rli.on('SIGINT', () => {
